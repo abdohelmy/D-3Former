7c9e6b1b98e93f4af3e4c9287ec8276d21e63148
Rushali Grandhe <rushaligrandhe@gmail.com>
2023-02-19 16:10:43 +0400

    Update eigen_cam.py

--- torch_grad_cam/eigen_cam.py
+++ torch_grad_cam/eigen_cam.py
@@ -1,9 +1,6 @@
 from torch_grad_cam.base_cam import BaseCAM
 from torch_grad_cam.utils.svd_on_activations import get_2d_projection
 
-# https://arxiv.org/abs/2008.00299
-
-
 class EigenCAM(BaseCAM):
     def __init__(self, model, target_layers, use_cuda=False,
                  reshape_transform=None):
